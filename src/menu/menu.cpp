3d38b9b28e4b0eed22379fdaf982571e2543cbb8
jwmartin0988 <jwmartin0988@gmail.com>
2016-09-05 01:36:50 -0500

    Update menu.cpp

--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -546,7 +546,7 @@ bool M_Responder (event_t *ev)
 			keyup = ev->type == EV_KeyUp;
 
 			ch = ev->data1;
-			Printf("%s\n", "Button Pressed")
+			Printf("%s\n", "Button Pressed");
 			Printf("%i\n", ch);
 			
 
